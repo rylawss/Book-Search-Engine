698770e848806c7549ac59131d83bc4666e8f092
rylawss <128633609+rylawss@users.noreply.github.com>
2023-09-17 21:14:04 -0500

    log connection

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,10 @@
-const mongoose = require('mongoose');
-
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks');
+const mongoose = require("mongoose");
+console.log(
+  "mongo connection string",
+  process.env.MONGODB_URI || "mongodb://localhost:27017/googlebooks"
+);
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost:27017/googlebooks"
+);
 
 module.exports = mongoose.connection;
